562ed43690d52c703cbfa0cdbcb24886dfb59601
Vineet Gadodia <vineet.gadodia@gmail.com>
2020-06-29 16:37:58 +0530

    Added prefix search functionality

--- algorithms/DS/addSearchWord.py
+++ algorithms/DS/addSearchWord.py
@@ -1,21 +1,29 @@
 import collections
 
 '''
-Design a data structure that supports the following two operations:
+Design a data structure that supports the following operations:
 
 void addWord(word)
 bool search(word)
 search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+prefixSearch(prefix) returns all words with prefix as prefix
 
 Example:
 
 addWord("bad")
+addWord("bat")
+addWord("bid")
+addWord("bit")
 addWord("dad")
 addWord("mad")
 search("pad") -> false
 search("bad") -> true
 search(".ad") -> true
 search("b..") -> true
+prefixSearch("b") -> ['bad', 'bat', 'bid', 'bit']
+prefixSearch("bi") -> ['bid', 'bit']
+prefixSearch("c") -> []
+
 Note:
 You may assume that all words are consist of lowercase letters a-z.
 
@@ -63,10 +71,27 @@ class Trie:
             else:
                 return self.search_helper(node.children[char], word, index+1)
         return True
-
+    
+    def prefixSearch(self, prefix):
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return []
+            current = current.children[char]
+        return self.prefixWords(current, [])
+    
+    def prefixWords(self, node, res):
+        if node.isWord:
+            res.append(node.word)
+        for child in node.children.values():
+            self.prefixWords(child, res)
+        return res
 
 t = Trie()
 t.addWord("bad")
+t.addWord("bat")
+t.addWord("bid")
+t.addWord("bit")
 t.addWord("dad")
 t.addWord("mad")
 print(t.search("pad") == False)
@@ -74,3 +99,6 @@ print(t.search("bad") == True)
 print(t.search(".ad") == True)
 print(t.search("b..") == True)
 print(t.search("b.d") == True)
+print(t.prefixSearch("b") == ['bad', 'bat', 'bid', 'bit'])
+print(t.prefixSearch("bi") == ['bid', 'bit'])
+print(t.prefixSearch("c") == [])
